e6f7de5802fd15e690e63acba1e5344c28e6af9a
Justin Sebastian <justinthomassebastian@gmail.com>
2022-04-01 09:52:34 -0600

    finish pizza exercise

---
+++ src/main/java/PizzaServlet.java
@@ -0,0 +1,41 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "PizzaServlet", urlPatterns = "/pizza")
+public class PizzaServlet extends HttpServlet {
+    protected List<String> toppingsList = new ArrayList<>();
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // get values from pizza order form
+        String crust = request.getParameter("crust");
+        String sauce = request.getParameter("sauce");
+        String size = request.getParameter("size");
+        String topping1 = request.getParameter("topping1");
+        String topping2 = request.getParameter("topping2");
+        String topping3 = request.getParameter("topping3");
+        String address = request.getParameter("address");
+
+        // add topping to toppings list if not null
+        addTopping(topping1);
+        addTopping(topping2);
+        addTopping(topping3);
+
+        // Output values
+        System.out.println("Crust: " + crust);
+        System.out.println("Sauce: " + sauce);
+        System.out.println("Size: " + size);
+        System.out.println("Toppings:" + toppingsList.toString());
+        System.out.println("Address: " + address);
+    }
+
+    protected void addTopping(String topping){
+        if(topping != null) {
+            toppingsList.add(topping);
+        }
+    }
+}
---
+++ src/main/webapp/pizza-order.jsp
@@ -0,0 +1,43 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Pizza</title>
+</head>
+<body>
+
+<form method="POST" action="/pizza">
+    <label for="crust">Crust Type:</label>
+    <select name="crust" id="crust">
+        <option value="stuffed">Stuffed Crust</option>
+        <option value="thick">Thick</option>
+        <option value="thin">Thin</option>
+    </select><br>
+
+    <label for="sauce">Sauce Type:</label>
+    <select name="sauce" id="sauce">
+        <option value="regular">Regular</option>
+        <option value="garlic">Garlic</option>
+        <option value="marinara">Marinara</option>
+    </select><br>
+
+    <label for="size">Size:</label>
+    <select name="size" id="size">
+        <option value="small">Small</option>
+        <option value="medium">Medium</option>
+        <option value="large">Large</option>
+    </select><br>
+
+    <label>Toppings: </label><br>
+    <input type="checkbox" id="topping1" name="topping1" value="pepperoni">
+    <label for="topping1"> Pepperoni</label><br>
+    <input type="checkbox" id="topping2" name="topping2" value="anchovies">
+    <label for="topping2"> Anchovies</label><br>
+    <input type="checkbox" id="topping3" name="topping3" value="pineapple">
+    <label for="topping3"> Pineapple</label><br>
+
+    <label for="address">Address:</label>
+    <input type="text" id="address" name="address"><br>
+    <input type="submit" value="Order">
+</form>
+</body>
+</html>
