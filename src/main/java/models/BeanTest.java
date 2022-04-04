79550c9bfc67678eea51e5271585a7f25ee89c13
Justin Sebastian <justinthomassebastian@gmail.com>
2022-04-04 09:25:59 -0600

    finish javabeans exercise

---
+++ src/main/java/models/BeanTest.java
@@ -0,0 +1,57 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class BeanTest {
+    public static void main(String[] args){
+        Album album1 = new Album();
+        Author author1 = new Author();
+        Author author2 = new Author();
+        Author author3 = new Author();
+        Quote quote1 = new Quote();
+        Quote quote2 = new Quote();
+        Quote quote3 = new Quote();
+
+        album1.setArtist("Gojira");
+        album1.setGenre("Metal");
+        album1.setId(1);
+        album1.setName("Fortitude");
+        album1.setSales(10.5F);
+        album1.setReleaseDate(new Date(1335765600L));
+
+        author1.setId(1);
+        author1.setFirstName("Brandon");
+        author1.setLastName("Sanderson");
+        author2.setId(2);
+        author2.setFirstName("George");
+        author2.setLastName("Martin");
+        author3.setId(3);
+        author3.setFirstName("Justin");
+        author3.setLastName("Sebastian");
+
+        quote1.setId(1);
+        quote1.setAuthorId(1);
+        quote1.setContent("The purpose of a storyteller is not to tell you how to think, but to give you questions to think upon.");
+        quote2.setId(2);
+        quote2.setAuthorId(2);
+        quote2.setContent("A reader lives a thousand lives before he dies, said Jojen. The man who never reads lives only one.");
+        quote3.setId(3);
+        quote3.setAuthorId(3);
+        quote3.setContent("I'm hungry.");
+
+        ArrayList<Quote> quotes = new ArrayList<>();
+        ArrayList<Author> authors = new ArrayList<>();
+        quotes.add(quote1);
+        quotes.add(quote2);
+        quotes.add(quote3);
+        authors.add(author1);
+        authors.add(author2);
+        authors.add(author3);
+
+        for(int i = 0; i < quotes.size(); i++){
+            System.out.println("Author: " + authors.get(i).getFirstName() + " " + authors.get(i).getLastName());
+            System.out.println("Quote: " + quotes.get(i).getContent());
+        }
+    }
+}
