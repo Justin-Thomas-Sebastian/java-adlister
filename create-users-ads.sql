7bec99cb56595ce997eb8226acc8590d5bf3f009
Justin Sebastian <justinthomassebastian@gmail.com>
2022-04-06 20:39:21 -0600

    add config and migration file. gitignore test

---
+++ create-users-ads.sql
@@ -0,0 +1,21 @@
+USE adlister_db;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS ads;
+
+CREATE TABLE users (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(200) NOT NULL,
+    password VARCHAR(200) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE ads(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id INT UNSIGNED NOT NULL,
+    title VARCHAR(200),
+    description TEXT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
